b40185c985b05e59d9d745a3e94810d9e90b26ab
ilpeiris <30365503+ilpeiris@users.noreply.github.com>
2025-11-05 23:10:21 +0530

    Phase 2: Model Java Classes

---
+++ src/main/java/com/fitlife/User.java
@@ -0,0 +1,36 @@
+package com.fitlife;
+
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String password; // i will only use this for registration
+
+    // getters and setters
+  
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/fitlife/Workout.java
@@ -0,0 +1,80 @@
+package com.fitlife;
+
+
+public class Workout {
+
+    private int workoutId;
+    private int userId; // link this workout to a user
+    private String activityType;
+    private int durationMins;
+    private double distanceKm;
+    private int caloriesBurned;
+    private String workoutDate; //  will use a simple txt date 
+    private String notes;
+
+    // getters and setters ---
+
+    public int getWorkoutId() {
+        return workoutId;
+    }
+
+    public void setWorkoutId(int workoutId) {
+        this.workoutId = workoutId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
+    }
+
+    public int getDurationMins() {
+        return durationMins;
+    }
+
+    public void setDurationMins(int durationMins) {
+        this.durationMins = durationMins;
+    }
+
+    public double getDistanceKm() {
+        return distanceKm;
+    }
+
+    public void setDistanceKm(double distanceKm) {
+        this.distanceKm = distanceKm;
+    }
+
+    public int getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public void setCaloriesBurned(int caloriesBurned) {
+        this.caloriesBurned = caloriesBurned;
+    }
+
+    public String getWorkoutDate() {
+        return workoutDate;
+    }
+
+    public void setWorkoutDate(String workoutDate) {
+        this.workoutDate = workoutDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
